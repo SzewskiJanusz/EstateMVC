a65c86bc5b69da19e86e54dc664648b898256ffc
Aleksander Januszewski <alex.januszewski@interia.pl>
2017-11-20 09:19:55 +0100

    added on click to table row in Result view and InformationController with simple view

--- EstateMVC/Controllers/InformationController.cs
+++ EstateMVC/Controllers/InformationController.cs
@@ -1,17 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
 
 namespace EstateMVC.Controllers
 {
     public class InformationController : Controller
     {
+        Models.EstateAgencyEntities db = new Models.EstateAgencyEntities();
         // GET: Information
         public ActionResult Index()
         {
             return View();
+        }
+
+        // GET: Row clicked
+        public ActionResult ShowProfile(string hlcn)
+        {
+            Models.Listing estate = 
+                db.Listing.Where(x => x.HLCN == hlcn).First();
+            long id = estate.ListingID;
+            string imagePath =
+                db.ListingPicture.Where(x =>
+                x.ListingID == id).Select(x => x.ImagePath).First();
+
+            Models.DetailedListing dl = new Models.DetailedListing
+            {
+                listing = estate,
+                imagePath = imagePath
+            };
+
+
+            return View(dl);
         }
     }
 }
---
+++ EstateMVC/Models/DetailedListing.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EstateMVC.Models
+{
+    public partial class DetailedListing
+    {
+        public EstateMVC.Models.Listing listing { get; set; }
+        public string imagePath { get; set; }
+    }
+}
